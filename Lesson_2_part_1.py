201c64d498db65a6d6ed06e016deb67152241111
sonzza <sonzza.life@gmail.com>
2019-09-11 12:26:38 +0300

    исправлен синтаксис

--- Lesson_2_part_1.py
+++ Lesson_2_part_1.py
@@ -14,6 +14,6 @@ my_list_1 = [2, 5, 8, 2, 12, 12, 4, 5, 55, 77, 8, 8, 3, 7]
 my_list_2 = [2, 7, 12, 3, 77, 8, 56]
 
 for my_objective in my_list_2:
-        while my_objective in my_list_1:
-            my_list_1.remove(my_objective)
+    while my_objective in my_list_1:
+        my_list_1.remove(my_objective)
 print(my_list_1)
