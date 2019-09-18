7083fb3f6fd023d55c5a6542fc366705d3734a55
sonzza <sonzza.life@gmail.com>
2019-09-18 23:10:30 +0300

    add lessons 8

--- main.py
+++ main.py
@@ -1,13 +1,54 @@
-# Создайте модуль main.py. Из модулей реализованных в заданиях 1 и 2 сделайте импорт в main.py всех функций. Вызовите
-# каждую функцию в main.py и проверьте что все работает как надо.
+import os
+import sys
 
+from core import delete_file, safe_info, create_folder, create_file, copy_file, get_list, change_dir, game
 
-import Lesson_5_part_1
-from Lesson_5_part_2 import random_func
+safe_info('старт')
 
-Lesson_5_part_1.add_dir()
-Lesson_5_part_1.del_dir()
+command = sys.argv[1]
+if command == 'list':
+    get_list()
+elif command == 'create_file':
+    try:
+        name = sys.argv[2]
+    except IndexError:
+        print('отсутствует название файла')
+    else:
+        create_file(name)
+elif command == 'create_folder':
+    try:
+        name = sys.argv[2]
+    except IndexError:
+        print('отсутствует название папки')
+    else:
+        create_folder(name)
+elif command == 'copy':
+    try:
+        name = sys.argv[2]
+        new_name = sys.argv[3]
+    except IndexError:
+        print('отсутствует название файла/папки для копирования или название нового файла/папки')
+    else:
+        copy_file(name, new_name)
+elif command == 'delete':
+    try:
+        name = sys.argv[2]
+    except IndexError:
+        print('отсутствует название папки')
+    else:
+        delete_file(name)
+elif command == 'change_dir':
+    name = sys.argv[2]
+    change_dir(name)
+    print(os.getcwd())
+elif command == 'game':
+    game()
+elif command == 'help':
+    print('list - список файлов и папок')
+    print('create_file - создание файла')
+    print('create_folder - создание папки')
+    print('copy - копирование файла или папки')
+    print('delete - удаление файла или папки')
+    print('change_dir - смена директории')
 
-test_mas = [5, 15, 10, 25, 12, 45]
-
-print(random_func(test_mas))
+safe_info('конец')
