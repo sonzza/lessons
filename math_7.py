1e8db88466fa1cd7251e63d12aa78864d7af4c46
sonzza <sonzza.life@gmail.com>
2019-11-13 19:05:30 +0300

    add 7

--- math_7.py
+++ math_7.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.linalg
 import matplotlib.pyplot as plt
 
 # 1. Решите линейную систему:
@@ -11,17 +12,24 @@ print(np.linalg.solve(A, B))
 C = ([[1, 2, -1], [3, -4, 0], [8, -5, 2], [2, 0, -5], [11, 4, -7]])
 D = ([1, 7, 12, 7, 15])
 print(np.linalg.lstsq(C, D))
+a = np.dot(C, [1.13919353, -0.90498444, -0.9009803])
+print(np.linalg.norm(a))
 
+# 3. Сколько решений имеет линейная система:
 E = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-F = np.array([12, 2, 48])
+F = np.array([12, 2, 1])
+# print(np.linalg.solve(E, F))
+
+# 4. Вычислите LU-разложение матрицы:
+G = np.array([[1, 2, 3], [2, 16, 21], [4, 28, 73]])
+P, L, U = scipy.linalg.lu(G)
+print(P)
+print(L)
+print(U)
+H = np.array([6, 39, 105])
+print(np.linalg.solve(G, H))
 
 
-print(np.linalg.lstsq(E, F))
 # 5. Найдите нормальное псевдорешение недоопределенной системы:
 
-G = np.array([[1, 2, -1], [8, -5, 2]])
-H = np.array([1, 12])
-def A():
-    return (np)
-x = np.linspace(-10, 100, 200)
-plt.plot()
+
